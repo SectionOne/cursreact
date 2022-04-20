a7422e55d334779c0dcbc6f6ad9caf09fc071ad2
Section1 <otinoco@digiocio.com>
2022-04-20 19:32:36 +0200

    CODE 4 - Exemple d'expressions js en JSX

--- cursreact/src/App.js
+++ cursreact/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
+function sumar(){
+  return 2 + 2;
+}
+
+const nom = "Joan";
+
 function App() {
   return (
     <>
-      <h1 className="color">Hola k ase?</h1>
-      <h1> Mercadona o k ase? </h1>
+      <div className="red">
+        <h2>Children</h2>
+      </div>
+      <h1 className="red"> Funcions { sumar() }</h1>
+      <h1> Operacions Matemàtiques { 3 + 2}</h1>
+      <h1> String { `Hola ${nom}` }</h1>
+      <h1> Logica / Ternary { ( 3 + 2 ) > 6 ? "mayor" : "menor" }</h1>
       <img src="" alt="" />
     </>
   );
