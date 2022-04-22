0e2d13791e67e279b3665cea32eb8d56f061f887
Section1 <otinoco@digiocio.com>
2022-04-22 18:46:02 +0200

    CODE 9 - Exemple de component funcional

--- cursreact/src/Header.js
+++ cursreact/src/Header.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import logo from './qx7jYSYm_400x400.jpg';
+import Salutacio from './Salutacio.js';
  class Header extends Component {
-
   render() {
+    var usuari = "Oriol";
     return(
-      <h1><img src={logo} height="50px" /> El teu portal de montanya.</h1>
+      <h1><img src={logo} height="50px" /> El teu portal de montanya.  <Salutacio nick={usuari} /> </h1>
     )
   }
 
---
+++ cursreact/src/Salutacio.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Menu({nick}){
+    return (<span>Benvingut/da {nick}</span>);
+}
+
+export default Menu;
+
