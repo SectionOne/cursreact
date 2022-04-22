224f04a4b92900f74d87f24f0664950a7fa6bd93
Section1 <otinoco@digiocio.com>
2022-04-22 19:56:52 +0200

    CODE 11 - Arxius d'estils locals

--- cursreact/src/components/Header.js
+++ cursreact/src/components/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './Header.css';
 import logo from '../img/qx7jYSYm_400x400.jpg';
 import Salutacio from './Salutacio.js';
  class Header extends Component {
   render() {
     var usuari = "Oriol";
     return(
-      <h1><img src={logo} height="50px" /> El teu portal de montanya.  <Salutacio nick={usuari} /> </h1>
+      <h1 className="salutacio"><img src={logo} height="50px" /> El teu portal de montanya.  <Salutacio nick={usuari} /> </h1>
     )
   }
 
