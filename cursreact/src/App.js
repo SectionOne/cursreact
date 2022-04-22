fed68b4617913f6ecd1fd2e87997d0c37f0acd29
Section1 <otinoco@digiocio.com>
2022-04-22 16:15:34 +0200

    CODE 8 - Exemple de component de classe

--- cursreact/src/App.js
+++ cursreact/src/App.js
@@ -1,22 +1,11 @@
 import './App.css';
+import Header from './Header.js';
 
 function App() {
 
-  const functionClick = () => {
-    console.log("onClick");
-  }
-
-  const functionOnMouseOver = () => {
-    console.log("onMouseOver");
-  }
-
-  const functionWithParameters = ( parameter ) => {
-    console.log(`function llençada desde: ${parameter} `);
-  }
   return (
     <>
-      <button onClick = { () => functionWithParameters("onClick") } > On Click </button>
-      <h1 onMouseOver = { () => functionWithParameters("mouseOver") } > Mouse Over Event </h1>
+      <Header/>
     </>
   );
 }
---
+++ cursreact/src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './qx7jYSYm_400x400.jpg';
+ class Header extends Component {
+
+  render() {
+    return(
+      <h1><img src={logo} height="50px" /> El teu portal de montanya.</h1>
+    )
+  }
+
+}
+
+export default Header
