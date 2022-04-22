3c8f29c15b068d318c79e3d64e069eaf2ab863a9
Section1 <otinoco@digiocio.com>
2022-04-22 12:53:50 +0200

    CODE 6 - Exemple control d'events classics

--- cursreact/src/App.js
+++ cursreact/src/App.js
@@ -1,28 +1,18 @@
 import './App.css';
 
-function sumar(){
-  return 2 + 2;
-}
-
-const nom = "Joan";
-
-const inLineStyle = {
-  color: "#2ecc71",
-  fontSize: "20px",
-}
-
 function App() {
+
+  const functionClick = () => {
+    console.log("onClick");
+  }
+
+  const functionOnMouseOver = () => {
+    console.log("onMouseOver");
+  }
   return (
     <>
-      <p style={inLineStyle}>InLineStyle</p>
-      <div className="red">
-        <h2>Children</h2>
-      </div>
-      <h1 className="red"> Funcions { sumar() }</h1>
-      <h1> Operacions Matemàtiques { 3 + 2}</h1>
-      <h1> String { `Hola ${nom}` }</h1>
-      <h1> Logica / Ternary { ( 3 + 2 ) > 6 ? "mayor" : "menor" }</h1>
-      <img src="" alt="" />
+      <button onClick = { functionClick } > On Click </button>
+      <h1 onMouseOver = { functionOnMouseOver } > Mouse Over Event </h1>
     </>
   );
 }
