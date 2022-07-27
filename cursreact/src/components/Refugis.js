cd8ff7cd65b25b29598f537ab00982cd988e5543
Section1 <otinoco@digiocio.com>
2022-07-27 19:58:21 +0200

    CODE 20 - Renderitzat de múltiples components

--- cursreact/src/components/Refugis.js
+++ cursreact/src/components/Refugis.js
@@ -3,9 +3,10 @@ import { useParams } from 'react-router-dom';
 
 function Refugis(){
    const nombres = [1, 2, 3, 4, 5];
-   const dobles = nombres.map((nombre) => nombre * 2);
-   console.log(doble);
-    
+   const ListRefus = nombres.map((nombre) => 
+    <li><a href={`/refugis/${nombre}`}>Refugi {`${nombre}`}</a></li>
+   );
+
     //Guardem en el console log els parametres que rebem
     console.log("useParams", useParams());
     const { id } = useParams();
@@ -13,8 +14,7 @@ function Refugis(){
         <>
         <h1>Llista de refugis</h1>
         <ul>
-            <li><a href="/refugis/1">Refugi 1</a></li>
-            <li><a href="/refugis/2">Refugi 2</a></li>
+            {ListRefus}
         </ul>
         <h2>El refugi seleccionat és { id }</h2>
         </>
