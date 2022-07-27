edf6826ed8b6df1d53469d74a9100ef021b9f4fc
Section1 <otinoco@digiocio.com>
2022-07-27 20:14:13 +0200

    CODI 22 - Llistes i Keys amb API

--- cursreact/src/components/Rutes.js
+++ cursreact/src/components/Rutes.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Rutes(){
+    const [articles, setArticles] = useState([]);
+    useEffect(() => {
+        fetch("https://scratchya.com.ar/react/datos.php")
+        .then((response) => {
+            return response.json();
+        })
+        .then((articles) => {
+            setArticles(articles);
+        })
+    }, [])
     return (
-        <h1>Llista de rutes</h1>
+        <>
+            <h1>Llista de rutes</h1>
+            <div>
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th>CODI</th>
+                            <th>DESCRIPCIO</th>
+                            <th>PREU</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {articles.map(art => {
+                            return(
+                                <tr key={art.codigo}>
+                                    <td>{art.codigo}</td>
+                                    <td>{art.descripcion}</td>
+                                    <td>{art.precio}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
 
