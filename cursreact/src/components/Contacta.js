2349b26a38804eeff9dde8238d0ec63181daa12d
Section1 <otinoco@digiocio.com>
2022-07-27 20:27:54 +0200

    CODI 24 - Formularis amb handles i useState

--- cursreact/src/components/Contacta.js
+++ cursreact/src/components/Contacta.js
@@ -1,25 +1,49 @@
 import React from 'react';
 
 const MyForm = () => {
+    const [values, setValues] = React.useState({
+      name: ''
+    })
   
-  return (
-    <div>
-      <h1>Formulari simple</h1>
-      <form>
+    const handleChange = (e) => {
+      const { name, value } = e.target
+      setValues({ ...values, [name]: value })
+    }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault()
+  
+      alert(JSON.stringify(values))
+    }
+  
+    const { name } = values
+  
+    
+    return (
+      <div>
+        <h1>Formulari simple</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+              Nom:
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+  
+          <button type="submit">Send</button>
+        </form>
+  
         <div>
-          <label>
-            Nom:
-            <input
-              type="text"
-              name="name"
-            />
-          </label>
+          <h2>Values of the form</h2>
+          <p>{JSON.stringify(values)}</p>
         </div>
-
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  )
+      </div>
+    )
 }
 
 export default MyForm;
